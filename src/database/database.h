92ea31c7aba7d96e2f1aee85052fb11b6a77f23e
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2025-08-04 15:55:25 -0700

    chore: add todo to create load game member function

--- src/database/database.h
+++ src/database/database.h
@@ -9,7 +9,9 @@ class Database {
   bool Open(const std::string& db_path);
   void Close();
   void SaveGame(Character* player, Character* enemy);
-
+  // TODO: Create `LoadGame()` member function to load game state from the 
+  // database. Add this once there is a UI to load a saved game before starting
+  // a new game.
  private:
   sqlite3* database_;
 };
